a198b4b3ae5a63bc87f2cd8c0d287cc4b77a00e9
Konstantinos Konstantinou <kkonnou@hotmail.com>
2018-09-27 11:52:31 -0400

    ok

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers {
+    [Authorize]
     
     [Route ("api/[controller]")]
     [ApiController]
@@ -17,6 +19,7 @@ namespace DatingApp.API.Controllers {
 
         }
         // GET api/values
+      
         [HttpGet]
         public async Task<IActionResult> GetValues () {
            var Values = await _ctx.Values.ToListAsync();
@@ -24,6 +27,7 @@ namespace DatingApp.API.Controllers {
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet ("{id}")]
         public async Task<IActionResult> GetValue (int id) {
             var value = await _ctx.Values.FirstOrDefaultAsync(x => x.Id == id);
