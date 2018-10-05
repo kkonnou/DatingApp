4e648e96461197366163d4a93fcb165cc598210d
Konstantinos Konstantinou <kkonnou@hotmail.com>
2018-10-05 15:39:11 -0400

    Chapter 9.6 completed

---
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -0,0 +1,28 @@
+import { AlertifyService } from './../../_Services/alertify.service';
+import { UserService } from './../../_Services/user.service';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../_models/user';
+
+@Component({
+  selector: 'app-member-list',
+  templateUrl: './member-list.component.html',
+  styleUrls: ['./member-list.component.css']
+})
+export class MemberListComponent implements OnInit {
+  users: User[];
+
+  constructor(private userService: UserService, private alertify: AlertifyService) { }
+
+  ngOnInit() {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService.getUsers().subscribe((users: User[]) => {
+      this.users = users;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+
+}
